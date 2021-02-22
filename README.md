2dcfaf518c3c3fda4203cd60ae72a8246e4e15bc
Evan Vander Stoep <38410002+EvanVS@users.noreply.github.com>
2021-02-21 21:04:41 -0800

    v.2.1.1

---
+++ README.md
@@ -0,0 +1,104 @@
+# OpenEndec V2.1
+
+> [!WARNING]
+> **DO NOT RELY ON THIS PROGRAM WHEN LOSS, DAMAGE, INJURY OR DEATH MAY OCCUR!**
+> **ALWAYS, ALWAYS HAVE MULTIPLE METHODS TO RECEIVE EMERGENCY ALERTS.**
+>
+> THIS IS *NOT* AN EAS DECODER
+
+An Open Source EAS ENDEC Logger to transmit News Feed messages recieved by a Sage Digital ENDEC Hardware Unit to a webhook/external destination.
+
+News Feed spec: [section 9.4 (pg. 70) of the ENDEC manual](https://www.sagealertingsystems.com/docs/digital_endec_1_0.pdf)
+
+EAS protocol specification: [47 CFR 11.31](https://www.ecfr.gov/current/title-47/chapter-I/subchapter-A/part-11/subpart-B/section-11.31)
+
+## Features
+
+> - [x] Supports multiple concurrent transmission destinations
+> - [ ] Argument validation
+
+**Destinations:**
+
+> - [x] Generic webhook
+> - [x] GroupMe
+> - [ ] Discord
+> - [ ] Email (SMTP)
+> - [ ] Twilio
+> - [ ] ???
+
+## Options
+
+`--com / -c`: The device to read from. This is the `/dev/` port for your ENDEC hardware. For example, if your ENDEC is connected to `/dev/ttyUSB0`, you would use `-c /dev/ttyUSB0`.
+
+`--trim / -t`: Trim the EAS message from the body before sending, which is always the final line. EAS messages follow the [format](https://www.ecfr.gov/current/title-47/chapter-I/subchapter-A/part-11/subpart-B/section-11.31): `ZCZC-ORG-EEE-PSSCCC + TTTT-JJJHHMM-LLLLLLLL-`
+
+`--quiet / -q`: Trim the human readable message body, leaving ONLY the EAS message.
+
+### Destinations
+
+`--webhook / -w {URL_1, URL_2, ...}`: Webhook URL(s) to forward EAS messages to.
+
+`--groupme / -g {BOT_ID_1, BOT_ID_2, ...}`: GroupMe bot(s) to forward EAS messages to.
+
+## Installation
+
+**Example scenario:**
+
+1. In Sage's [EndecSetD](https://www.sagealertingsystems.com/support-pc.htm), set one of the DB-9 serial COM ports on your ENDEC to output "News Feed". See [section 9.4 (pg. 70) of the ENDEC manual](https://www.sagealertingsystems.com/docs/digital_endec_1_0.pdf) for more info.
+2. Use a [USB to RS-232 serial adapter](https://amzn.to/46FljxQ) to connect the News Feed COM port of your ENDEC to a computer. We're using a Raspberry Pi, though any computer that can run Python and connect to the internet should work fine.
+3. Run `sudo dmesg`  (for [macOS/Linux](https://man7.org/linux/man-pages/man1/dmesg.1.html)) to identify the the `/dev/` device the adapter is plugged into. We're looking for a new USB detection for a serial device. Using the RS-232 adapter linked above, this is the log we found:
+
+    ```sh
+    usb 1-1.1: FTDI USB Serial Device converter now attached to ttyUSB0
+    ```
+
+    This is telling us that the ENDEC feed will come in at `/dev/ttyUSB0`. If you're feeling up to it, assign the device a more user-friendly name by creating a udev rule. See [this guide](https://www.rigacci.org/wiki/doku.php/doc/appunti/linux/sa/renaming_usb_devices) for more info.
+
+4. Clone this repo and navigate to it via `git clone https://github.com/WBOR-91-1-FM/wbor-endec && cd wbor-endec`
+5. Install necessary dependencies via `pip install -r requirements.txt`, or, better yet, activate a virtual environment for this repo *and then* install the dependencies.
+6. Start monitoring the ENDEC by running the script:
+
+    ```sh
+    python3 OpenEndecV2.py -c {YOUR-DEVICE} {OPTIONS}
+    ```
+
+    {YOUR DEVICE} is the `/dev/` port we found in step 3. So, in this example, we use `/dev/ttyUSB0`.
+
+    [{OPTIONS}](#options) specifies the [destinations](#destinations) OpenEndec should forward EAS messages to.
+
+### Running as a System Service
+
+```sh
+sudo nano /etc/systemd/system/wbor-endec.service
+```
+
+Reference the template at `wbor-endec.service`.
+
+You will need to change:
+
+- `{DEVICE}` = the `/dev/` device you found in step 3, e.g. `/dev/ttyUSB0`. IMPORTANT NOTE: Systemd derives the device unit name from the path by converting slashes to dashes, e.g. /dev/ttyENDEC → dev-ttyENDEC.device. So, if your device is `/dev/ttyUSB0`, the unit name will be `dev-ttyUSB0.device`. You can check this by running `systemctl list-units --type=device` and looking for your device.
+- Under `ExecStart`
+  - `{PYTHON_EXEC}` = directory for your Python executable, e.g. `/usr/bin/python3` or, if using a virtual environment, `/home/username/wbor-endec/venv/bin/python`
+  - `{SCRIPT_PATH}` = path to `OpenEndecV2.py`, e.g. `/home/username/wbor-endec/OpenEndecV2.py`
+  - `{OPTIONS}` = options for the scripts with respective arguments, e.g. `--groupme {BOT_ID}`
+- `WorkingDirectory` = path to the `wbor-endec` repo folder you cloned, e.g. `/home/username/wbor-endec`
+- `User` = username for the user running OpenEndec (e.g. `pi` for Raspberry Pi)
+
+**Device Unit Naming:**
+
+After saving, run:
+
+```sh
+sudo systemctl daemon-reload
+sudo systemctl enable wbor-endec.service
+sudo systemctl start wbor-endec.service
+sudo systemctl status wbor-endec.service
+```
+
+Look for `active (running)` and the `openendec.log` file to confirm it is up and running.
+
+After updating, run:
+
+```sh
+sudo systemctl start wbor-endec.service
+```
---
+++ endec.py
@@ -0,0 +1,269 @@
+"""
+Decode NewsFeed EAS messages from a Sage Digital ENDEC and forward them
+to a webhook URL or a GroupMe group.
+
+Authors:
+    - Evan Vander Stoep <@evanvs>
+    - Mason Daugherty <@mdrxy>
+
+Version: 2.1.1
+Last Modified: 2025-03-23
+
+Changelog:
+    - 1.0.0 (????): Initial release <@evanvs>
+    - 2.0.0 (2021-02-22): Second release <@evanvs>
+    - 2.1.0 (2024-08-08): Refactored for better readability and added
+        support for GroupMe <@mdrxy>
+"""
+
+import argparse
+import json
+import logging
+import time
+
+import requests
+from serial import Serial
+from serial.serialutil import SerialException
+
+LOGFILE = "openendec.log"
+
+logging.basicConfig(
+    filename=LOGFILE,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s : %(message)s",
+)
+
+message_content = ""
+eas = ""
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-c",
+    "--com",
+    dest="port",
+    default="/dev/ttyUSB0",
+    help="Select the port the device is on. Default is /dev/ttyUSB0",
+)
+parser.add_argument(
+    "-w", "--webhook", dest="webhookUrls", nargs="+", help="Webhook URL(s) to send to."
+)
+parser.add_argument(
+    "-g",
+    "--groupme",
+    dest="groupmeBotId",
+    nargs="+",
+    help="Send ENDEC messages to a GroupMe Group. Pass in the bot ID to use.",
+)
+parser.add_argument(
+    "-d",
+    "--debug",
+    dest="debug",
+    action="store_true",
+    default=False,
+    help="Enable debug logging.",
+)
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument(
+    "-t",
+    "--trim",
+    dest="trim",
+    action="store_true",
+    default=False,
+    help=(
+        "Trim the EAS message from the body before sending, destroying it."
+        '"message" will contain the human readable text ONLY.'
+    ),
+)
+group.add_argument(
+    "-f",
+    "--fork",
+    dest="fork",
+    action="store_true",
+    default=False,
+    help=(
+        'Trim the EAS message from the body and send it as "eas" in the webhook '
+        'payload. "message" will contain the human readable text.'
+    ),
+)
+group.add_argument(
+    "-q",
+    "--quiet",
+    dest="quiet",
+    action="store_true",
+    default=False,
+    help=(
+        "Trim the human readable text from the message before sending. destroying it."
+        'ONLY the EAS message will be sent (as "message").'
+    ),
+)
+
+args = parser.parse_args()
+requiredArgs = {"webhook": "webhookUrls", "groupme": "groupmeBotId"}
+
+if not any(getattr(args, arg) for arg in requiredArgs.values()):
+    ARG_LIST = ", ".join([f"--{arg}" for arg in requiredArgs.keys()])
+    parser.error(
+        f"At least one of the following arguments must be provided: {ARG_LIST}"
+    )
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+
+class Webhook:
+    """
+    Generic class for sending messages to a webhook URL.
+    """
+
+    def __init__(self, url=None, eas=None):
+        self.headers = {"Content-Type": "application/json"}
+        self.url = url
+        self.eas = eas
+
+    def post(self, message_content):
+        """
+        Generic POST request to a webhook URL.
+
+        Parameters:
+        - message_content (str): The message to send to the webhook.
+        """
+        self.payload = {"message": message_content}
+
+        if self.eas:
+            self.payload["eas"] = self.eas
+
+        logging.info(
+            "Making POST to %s with payload: %s", self.url, json.dumps(self.payload)
+        )
+        response = requests.post(
+            self.url, headers=self.headers, json=json.dumps(self.payload), timeout=10
+        )
+        logging.info("Response from %s: %s", self.url, response.text)
+
+
+class GroupMe(Webhook):
+    """
+    Operations for sending messages to a GroupMe group via a Bot.
+    """
+
+    def post(self, message_content):
+        """
+        Post message to a GroupMe group via a Bot.
+        """
+
+        self.url = "https://api.groupme.com/v3/bots/post"
+
+        footer = (
+            "\n\nThis message was sent using OpenENDEC V2.1"
+            "[github/WBOR-91-1-FM/wbor-endec]\n----------"
+        )
+        body = f"{message_content}{footer}"
+
+        # Split body into 500 character segments (max length for GroupMe messages)
+        segments = [body[i : i + 500] for i in range(0, len(body), 500)]
+
+        for segment in segments:
+            # Forward to all bots specified
+            for bot_id in args.groupmeBotId:
+                # Schema: https://dev.groupme.com/docs/v3#bots_post
+                self.payload = {"bot_id": bot_id, "text": segment}
+
+                logging.debug("Making POST to GroupMe with payload: %s", self.payload)
+                logging.info("Making POST to GroupMe")
+                response = requests.post(
+                    self.url, headers=self.headers, json=self.payload, timeout=10
+                )
+                if response.text:
+                    logging.error("GroupMe's response: %s", response.text)
+                else:
+                    logging.info("GroupMe POST successful")
+
+
+def post():
+    """
+    Send News Feed object message payload to specified webhooks.
+
+    Raises:
+    - requests.exceptions.RequestException: If the request to a webhook
+        fails.
+    """
+    global message_content
+    global eas
+
+    # Post to each webhook URL provided
+    if args.webhookUrls:
+        for url in args.webhookUrls:
+            Webhook(url, eas).post(message_content)
+
+    # Post to GroupMe if bot ID is provided
+    if args.groupmeBotId:
+        GroupMe().post(message_content)
+
+    message_content = ""
+    eas = ""
+
+
+def newsfeed():
+    """
+    Continuously decodes News Feed objects from the provided serial
+    port.
+
+    Raises:
+    - serial.SerialException: If the serial connection fails.
+    """
+    serial_text = ""
+    data_list = []
+    global message_content
+    global eas
+    active_alert = False
+    i = 0
+
+    while True:
+        try:
+            ser = Serial(args.port, baudrate=9600, bytesize=8, stopbits=1)
+            logging.info("Connected to serial port %s", args.port)
+            if ser.isOpen():
+                while True:
+                    serial_text = ser.readline().decode("utf-8").strip()
+                    if "<ENDECSTART>" in serial_text:
+                        active_alert = True
+                    elif "<ENDECEND>" in serial_text:
+                        if args.trim:
+                            data_list.pop()
+
+                        if args.fork:
+                            eas = data_list.pop()
+
+                        if args.quiet:
+                            data_list = [data_list[-1]]
+
+                        message_content = "".join(data_list)
+                        data_list = []
+                        active_alert = False
+                        i = 0
+                        post()
+                    else:
+                        if active_alert:
+                            data_list.append(serial_text)
+                            logging.debug("Line #%d: %s", i, serial_text)
+                            i += 1
+        except (SerialException, requests.exceptions.RequestException) as e:
+            logging.error("Handled error: %s", e)
+        finally:
+            if ser.isOpen():
+                ser.close()
+                logging.info("Closed serial port %s", args.port)
+            logging.info("Reconnecting to serial port...")
+            time.sleep(5)  # Wait before trying to reconnect
+
+
+if __name__ == "__main__":
+    logging.info(
+        "OpenENDEC V2.1\n"
+        "Originally Written By: Evan Vander Stoep [https://github.com/EvanVS]\n"
+        "Modified by: Mason Daugherty [@mdrxy] for WBOR 91.1 FM [https://wbor.org]\n\n"
+        "Logger Started!\nLogs will be stored at %s",
+        LOGFILE,
+    )
+    newsfeed()
