f494f97a238c853107eddc42d37ceaa4ce86a7fe
Mason Daugherty <mdaugher@bowdoin.edu>
2024-08-07 00:41:30 -0400

    Add logging, don't exit script after first alert, decode binary string as utf-8

--- OpenEndecV2.py
+++ OpenEndecV2.py
@@ -1,6 +1,12 @@
-import os, json, sys, argparse, requests, serial
+import os, json, sys, argparse, requests, serial, time, logging
 from serial import Serial
 from serial.serialutil import SerialException
+
+logging.basicConfig(
+    filename="openendec.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 content = ""
 
@@ -15,7 +21,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "-w",
-    "--webhooks",
+    "--webhook",
     dest="webhook",
     nargs="+",
     default="",
@@ -24,15 +30,23 @@ parser.add_argument(
 )
 args = parser.parse_args()
 port = args.port
-webhooks = args.webhooks
+webhooks = args.webhook
 
 
 def main():
     global content
     payload = {"content": content}
+    logging.info("Payload: %s", payload)
+    logging.info("Sending to Webhooks: %s", webhooks)
+
     header_data = {"content-type": "application/json"}
     for webhook in webhooks:
-        requests.post(webhook, json.dumps(payload), headers=header_data)
+        try:
+            response = requests.post(webhook, json.dumps(payload), headers=header_data)
+            response.raise_for_status()
+            logging.info("Successfully posted to %s", webhook)
+        except requests.exceptions.RequestException as e:
+            logging.error("Failed to post to %s: %s", webhook, e)
     content = ""
 
 
@@ -40,27 +54,40 @@ def newsFeed():
     serialText = ""
     dataList = []
     global content
-    ser = serial.Serial(port=port, baudrate=9600, bytesize=8, stopbits=1)
-    if ser.isOpen():
-        while True:
-            serialText = str(ser.readline())
-            if "<ENDECSTART>" in serialText:
-                activeAlert = True
-            elif "<ENDECEND>" in str(serialText):
-                content = "".join(dataList)
-                del dataList[:]
-                activeAlert = False
-                main()
-                break
-            else:
-                if activeAlert == True:
-                    dataList.append(serialText)
-                else:
-                    pass
-    else:
-        print("Serial Port in use or non-existent.")
+    activeAlert = False
 
-print(
-    "OpenENDEC V2\nOriginally Written By: Evan Vander Stoep [https://github.com/EvanVS]\nModified by: Mason Daugherty [https://github.com/mdrxy] for WBOR 91.1 FM [https://wbor.org]\n\nLogger Started!"
-)
-newsFeed()
+    while True:
+        try:
+            ser = serial.Serial(port=port, baudrate=9600, bytesize=8, stopbits=1)
+            logging.info("Connected to serial port %s", port)
+            if ser.isOpen():
+                while True:
+                    serialText = ser.readline().decode("utf-8")
+                    if "<ENDECSTART>" in serialText:
+                        activeAlert = True
+                    elif "<ENDECEND>" in serialText:
+                        content = "".join(dataList)
+                        dataList = []
+                        activeAlert = False
+                        main()
+                    else:
+                        if activeAlert:
+                            dataList.append(serialText)
+        except SerialException as e:
+            logging.error("Serial exception: %s", e)
+        except Exception as e:
+            logging.error("Unexpected error: %s", e)
+        finally:
+            if ser.isOpen():
+                ser.close()
+                logging.info("Closed serial port %s", port)
+            logging.info("Reconnecting to serial port...")
+            # Sleep or wait before reconnecting
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    logging.info(
+        "OpenENDEC V2\nOriginally Written By: Evan Vander Stoep [https://github.com/EvanVS]\nModified by: Mason Daugherty [https://github.com/mdrxy] for WBOR 91.1 FM [https://wbor.org]\n\nLogger Started!"
+    )
+    newsFeed()
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# OpenEndec V2.0
-An Open Source EAS ENDEC Discord Webhook Logger
+# OpenEndec V2.1
 
-This repo contains the base python scripts to log alerts recieved by your Emergency Alert System Encoder/Decoder Hardware Unit to a Discord webhook.
+An Open Source EAS ENDEC Logger
 
-Edits/Forks are welcome! :)
+This repo contains the base python scripts to log alerts recieved by your Emergency Alert System Encoder/Decoder Hardware Unit to a webhook.
